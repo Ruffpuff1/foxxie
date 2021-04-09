7bbe56edfd5122851ba0485ed906dd3197e9224c
orihuu <81810945+orihuu@users.noreply.github.com>
2021-04-09 12:05:51 +0200

    pimg js

--- commands/ping.js
+++ commands/ping.js
@@ -9,7 +9,7 @@ module.exports = {
       Embed.setTitle(`Fokushi's Ping`)
       Embed.setDescription(`:ping_pong: Latency is ${Date.now() - message.createdTimestamp} ms`)
       Embed.setTimestamp()
-      Embed.setThumbnail(`https://images-ext-1.discordapp.net/external/lwJGAWKFCzOo0GwYPM9kVrT92uypza070vnT9Y9V5QA/%3Fsize%3D4096/https/cdn.discordapp.com/avatars/827514096865509386/0b6bcef7c10695b00e97d416c0e70c99.png?width=586&height=586`)
+      Embed.setThumbnail(message.guild.iconURL())
       Embed.setColor("#f59dcc")
   message.channel.send(Embed)
      }
