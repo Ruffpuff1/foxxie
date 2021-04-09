3070531985e734dbd804450d70ccf9d117efc387
orihuu <81810945+orihuu@users.noreply.github.com>
2021-04-09 11:01:53 +0200

    rrrrrrrrrrrrrrrrrrr

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ ${messageDelete.author.tag} `)
     Embed.setFooter('why are you reading this lmao');
     logChannel.send(Embed)
    });
-   bot.on("messageUpdate", (messageUpdate) => {
+   bot.on("messageUpdate", (oldMessage, newMessage) => {
     if (messageUpdate.guild.id !== "822187156214513734") return
     const logChannel = bot.channels.cache.get("828563803532296252")
     const Embed = new Discord.MessageEmbed()
@@ -67,7 +67,11 @@ ${messageDelete.author.tag} `)
 
 The Before Message:
 
-**${messageUpdate.content}**
+**${oldMessage.content}**
+
+The After Message:
+
+**${newMessage.content}
 
 The Author:
 
