2d383aa3d178e3eb314aa48d85d87d00e1b1dc8f
orihuu <81810945+orihuu@users.noreply.github.com>
2021-04-09 11:29:23 +0200

    ruffff

---
+++ ruffy.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'ruffy',
+    description: 'ruff lol',
+    aliases: ['ruff', 'reese', 'best coder'],
+    execute(message) {
+  message.channel.send(`<:PVulpixAngel:816747766406578234>`)
+  message.channel.send(`<:PUmbreonCoffee:816751699552894977>`)
+    }
+  }
