0e62b26d0ed13095b3e7f133261a033ebf910cc4
orihuu <81810945+orihuu@users.noreply.github.com>
2021-04-09 11:32:24 +0200

    woeigh

--- ruffy.js
+++ ruffy.js
@@ -2,6 +2,7 @@ module.exports = {
     name: 'ruffy',
     description: 'ruff lol',
     aliases: ['ruff', 'reese', 'best coder'],
+    
     execute(message) {
   message.channel.send(`<:PVulpixAngel:816747766406578234>`)
   message.channel.send(`<:PUmbreonCoffee:816751699552894977>`)
