c791ea6face17a1403f85d4dd4d157f7cd407d9d
orihuu <81810945+orihuu@users.noreply.github.com>
2021-04-09 17:09:43 +0200

    tainted

--- commands/ruffy.js
+++ commands/ruffy.js
@@ -4,7 +4,7 @@ module.exports = {
     aliases: ['ruff', 'reese', 'best coder'],
     
     execute(message) {
-  message.channel.send(`<:PVulpixAngel:816747766406578234>`)
-  message.channel.send(`<:PUmbreonCoffee:816751699552894977>`)
+  message.channel.send(`https://images-ext-2.discordapp.net/external/JMzvYBgeRZ5SAlgjadMhIdrrMhWPSCicWYFuj6CNbFU/https/cdn.discordapp.com/emojis/816751699552894977.png`)
+  message.channel.send(`https://images-ext-1.discordapp.net/external/3tB45e3JEeRM-UStsUCtoGbicEIq6apI6cGEwlqW9ps/https/cdn.discordapp.com/emojis/816747766406578234.png`)
     }
   }
