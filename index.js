250b26374c6f1fdac10288c4029fb424da874271
orihuu <81810945+orihuu@users.noreply.github.com>
2021-04-09 09:35:30 +0200

    jwegnejwkfn

--- index.js
+++ index.js
@@ -38,6 +38,11 @@ bot.on("guildMemberRemove", member => {
     Embed.setFooter('We will mourn them in general. | Left ');
     goodbyeChannel.send(Embed)
 })
+bot.on("messageDelete", (messageDelete) => {
+    const messageDelete = bot.channels.cache.get("828563803532296252")
+    messageDelete.channel.send(`The message : "${messageDelete.content}" by ${messageDelete.author.tag} was deleted.`)
+   });
+   
 
 /**
  * Import all commands
