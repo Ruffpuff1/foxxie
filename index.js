3df715dd87cb43e9dfe7ac30b14a44adf3203242
orihuu <81810945+orihuu@users.noreply.github.com>
2021-04-10 13:30:53 +0200

    yea

--- index.js
+++ index.js
@@ -90,10 +90,37 @@ The Author:
     logChannel.send(Embed)
    });
 
+   bot.on("guildMemberAdd", (member) => {
+    if (member.guild.id !== config.fox) return
+    const welcomeChannel = bot.channels.cache.get("822414098439864360")
+    const embed = new Discord.MessageEmbed()
+    embed.setColor('#ff8940')
+    embed.setTitle(`A new member appeared!`)
+    embed.setDescription(`Why, hello there, ${member} the cutie! Welcome to our little tropical paradise, located out in the middle of the ocean, Seaside Restaurant! Here’s some places you might want to visit first! Be sure to give the <#822189857593098310> channel a read, so we can keep peace on our little island. The <#822468803182329886> channel is where you can go to pick up some shiny roles for yourself. You can go to <#822454757151080489> to see our list of server staff. And when you’re finally ready, <#822193883726741565> is where you can go to meet the rest of the server’s friendly faces! Enjoy your stay! ^^ `)
+    embed.setImage(`https://media.discordapp.net/attachments/827515349959835708/829619448972312576/unknown.png`)
+    embed.setThumbnail(`https://images-ext-2.discordapp.net/external/5u_By7TVoV9u2SM7ujLOyVDELf-koOmg0AOvLYJq4_s/%3Fsize%3D4096/https/cdn.discordapp.com/icons/822187156214513734/098cbdf8e61a55d18e973afb4e2e292b.png`);
+    embed.setTimestamp()
+    embed.setFooter('Give them a warn welcome in general! | Joined ');
+    welcomeChannel.send('A new member joined! Give them a warm welcome please <@&829752715826823268>!')
+    welcomeChannel.send(embed)
+})
+bot.on("guildMemberRemove", (member) => {
+    if (member.guild.id !== config.fox) return
+    const goodbyeChannel = bot.channels.cache.get("822414098439864360")
+    const Embed = new Discord.MessageEmbed()
+    Embed.setColor('#990670')
+    Embed.setTitle(`A member continued their journey somewhere else...`)
+    Embed.setDescription(`Sadly, ${member} hopped on a boat and sailed far away, back across the ocean. Safe travels! There are ${member.guild.memberCount} customers left, dwelling the island.`)
+    Embed.setImage(`https://media.discordapp.net/attachments/829649102333149206/829654630132088842/94a0071504a8b2faf77ae446c56df37a.gif`)
+    Embed.setTimestamp()
+    Embed.setFooter('We will mourn them in general. | Left ');
+    goodbyeChannel.send(Embed)
+})
+
 bot.on("guildMemberAdd", (member) => {
     if (member.guild.id !== config.fox) return
     var role = member.guild.roles.cache.find(role => role.name == "test")
-    member.roles.add(role);
+    member.roles.add(role);v
 });
 
 
