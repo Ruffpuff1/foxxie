edf6d8aa3372a5fd5ecd280f1c54e7d4c50fab7a
orihuu <81810945+orihuu@users.noreply.github.com>
2021-04-12 14:38:34 +0200

    no mention avatar viewing

--- commands/avatar.js
+++ commands/avatar.js
@@ -2,31 +2,18 @@ const Discord = require('discord.js');
 module.exports = {
     name: 'avatar',
     aliases: ["av", "ava"],
-    description: 'Shows a users avatar ',
-    execute(message, args, bot) {
-        bot.users.cache.find(r => r.username.toLowerCase() === args.join(' ').toLocaleLowerCase())
-        const men =  message.mentions.members.first() || message.guild.members.cache.find(m => m.displayName == args[0]) || 
-message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(m => m.user.tag == args[0]) 
-        if(!men){
-            let embed = new Discord.MessageEmbed();
-            embed.setTitle(`${message.author.username}, your avatar.
-i hope you like it.
+    description: 'Shows a users avatar',
+    execute(message, args) {
+        let men = message.member;
+        if (args[0]) men =  message.mentions.members.first() || message.guild.members.cache.find(m => m.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || 
+        message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(m => m.user.tag.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member;
+       
+        let embed = new Discord.MessageEmbed()
+            .setTitle(`**${men.user.tag}**, ${men.user.id === message.member.user.id ? "Your avatar." : "Their avatar."}`)
+            .setTimestamp()
+            .setColor('#0057ab')
+            .setImage(men.user.displayAvatarURL({ format: "png", dynamic: true, size: 4096}));
 
-(ID: ${message.author.id}) `)
-            embed.setColor('#0057ab')
-            embed.setTimestamp()
-            embed.setImage(message.author.displayAvatarURL({ format: "png", dynamic: true, size: 4096}));
-            message.channel.send(embed)
-        } else { 
-            let embed = new Discord.MessageEmbed();
-            embed.setTitle(`${men.user.tag}, their avatar. 
-i hope you like it. 
-
-(ID: ${men.user.id})`)
-embed.setTimestamp()
-embed.setColor('#0057ab')
-            embed.setImage(men.user.displayAvatarURL({ format: "png", dynamic: true, size: 4096}));
-            message.channel.send(embed)
-        }
+        message.channel.send(embed)
     }
 }
