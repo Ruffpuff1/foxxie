b81a3f635c56a4eb4b6dd08a8c80da71b9e0e1d5
orihuu <81810945+orihuu@users.noreply.github.com>
2021-04-12 18:13:42 +0200

    despacito

--- commands/eval.js
+++ commands/eval.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const moment = require('moment')
-const config = require('./config.json')
+const config = require('../config.json')
 
 module.exports = {
     name: 'eval',
