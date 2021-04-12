8d019d8ca830c842f3db3d997eeb653a9c4cc3fc
orihuu <81810945+orihuu@users.noreply.github.com>
2021-04-12 10:14:21 +0200

    se

--- commands/stealemote.js
+++ commands/stealemote.js
@@ -4,12 +4,12 @@ module.exports = {
     description: "A command you can use to easily add emotes to your server.",
     cooldown: 3,
     execute(message, args) { 
-      if (!args[0]) { return message.channel.send('Well yknow, thats not correct. you have to do \`hk se (emote/image`)\` thanks.'); } 
-      if (!args[1]) { return message.channel.send('Well yknow, thats not correct. you have to do \`hk se (emote/image`)\` thanks.'); }  
+      if (!args[0]) { return message.channel.send('Well yknow, thats not correct. you have to do \`hk se (emote/image`)\` (name) thanks.'); } 
+      if (!args[1]) { return message.channel.send('Well yknow, thats not correct. you have to do \`hk se (emote/image`)\` (name) thanks.'); }  
        const regex = /<(a?):(\w+):(\d+)>/
        const emotee = (`${args[0]}`).match(regex)
        const image = (`${args[0]}`).match(/\b(https?:\/\/\S+(png|jpe?g|gif)\S*)\b/i)
-      if (!emotee && !image) { return message.channel.send('Well yknow, thats not correct. you have to do \`hk se (emote/image`)\` thanks.'); }
+      if (!emotee && !image) { return message.channel.send('Well yknow, thats not correct. you have to do \`hk se (emote/image`)\` (name) thanks.'); }
       if (image) {
       if (image[2] === 'gif') { message.guild.emojis.create(args[0], args[1]).then(emoji => message.channel.send(`ey, i added a new emote for ya love: <a:${emoji.name}:${emoji.id}>`)).catch(console.error); }
       else { message.guild.emojis.create(args[0], args[1]).then(emoji => message.channel.send(`ey, i added a new emote for ya love: <:${emoji.name}:${emoji.id}>`)).catch(console.error); } }    
