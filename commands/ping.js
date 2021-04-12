5ed130656c85ee64e9db54a4bf6589628400f481
orihuu <81810945+orihuu@users.noreply.github.com>
2021-04-12 10:13:31 +0200

    ping

--- commands/ping.js
+++ commands/ping.js
@@ -1,29 +1,18 @@
-const { MessageEmbed, Channel } = require('discord.js')
+const Discord = require('discord.js');
 
 module.exports = {
-  name: 'ping',
-  aliases: ['pong'],
-  description: 'to check the bots ping.',
-
-  callback: ({ message, client, interaction }) => {
-    const embed = new MessageEmbed().setTitle(':ping_pong: Pong Counters!').setColor(3553598).addFields({
-      name: '**API Latency**',
-      value: '```'+`${Math.round(client.ws.ping)} ms`+'```',
-      inline: true
-    })
-
-    if (message) { if (!message.author.bot) {
-      const embed = new MessageEmbed().setTitle(':ping_pong: Pong Counters!').setColor(3553598).addFields({
-        name: '**Bot Latency**',
-        value: '```'+`${Date.now() - message.createdTimestamp} ms`+'```',
-        inline: true
-      }, {
-        name: 'API Latency',
-        value: '```'+`${Math.round(message.client.ws.ping)} ms`+'```',
-        inline: true
-      })	
-      message.channel.send(embed)
-    } }
-    return embed
-  },
-}
+    name: 'ping',
+    description: 'Shows the latency of the bot.',
+    aliases: ['pong', 'pimg'],
+    execute(message) {
+      let Embed = new Discord.MessageEmbed()
+      Embed.setTitle(`Fokushi's Ping`)
+      Embed.setDescription(`\`🏓 Latency is ${Date.now() - message.createdTimestamp} ms\``)
+      Embed.setTimestamp()
+      Embed.setFooter(`Discord is shitty, so the ping may be high due to that. 
+  Sorry.`)
+      Embed.setThumbnail(message.guild.iconURL())
+      Embed.setColor("#66b3ff")
+  message.channel.send(Embed)
+     }
+  }
