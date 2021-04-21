19f708e01c2cdcbbd2c54edd13f0ec4e2f5dbc92
orihuu <81810945+orihuu@users.noreply.github.com>
2021-04-21 14:08:22 +0200

    uptime again sorry for the spam

--- commands/nick.js
+++ commands/nick.js
@@ -17,3 +17,4 @@ module.exports = {
         nickchannel.send(Embed)
     }
 }
+
--- commands/uptime.js
+++ commands/uptime.js
@@ -5,15 +5,29 @@ module.exports = {
     description: 'Tells you the uptime of me.',
     aliases: ['ut', 'up'],
     execute(message, args, bot) {
-        let totalSeconds = (bot.uptime / 1000);
-        let days = Math.floor(totalSeconds / 86400);
-        totalSeconds %= 86400;
-        let hours = Math.floor(totalSeconds / 3600);
-        totalSeconds %= 3600;
-        let minutes = Math.floor(totalSeconds / 60);
-        let seconds = Math.floor(totalSeconds % 60);
-        
-        let uptime = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
+        let days = 0
+let week = 0
+let uptime = '';
+let totalSeconds = (bot.uptime / 1000)
+let hours = Math.floor(totalSeconds / 3600)
+totalSeconds %= 3600
+let minutes = Math.floor(totalSeconds / 60)
+let seconds = Math.floor(totalSeconds % 60)
+if (hours > 24) {
+days = days + 1
+hours = 0
+        }
+if (week - 0) {
+uptime += `${week} week, `
+        }
+if (minutes > 60) {
+minutes = 0;
+        }
+uptime += `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`
+
+
+
+message.channel.send(`my uptime is ${uptime}`)
 
     }
 }
