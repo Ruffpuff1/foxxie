3125cda63afb7064f9e3a3849d33d8f6d98380a1
orihuu <81810945+orihuu@users.noreply.github.com>
2021-04-21 16:51:41 +0200

    nick

--- commands/nick.js
+++ commands/nick.js
@@ -14,6 +14,7 @@ module.exports = {
         Embed.setTimestamp()
         Embed.setFooter(`${message.author.tag}`)
         Embed.setColor(`Random`);
+        
         nickchannel.send(Embed)
     }
 }
