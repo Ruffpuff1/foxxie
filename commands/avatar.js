71fb29266ae96380285e358895e5c6b392f92c4b
orihuu <81810945+orihuu@users.noreply.github.com>
2021-04-22 09:10:55 +0200

    av

--- commands/avatar.js
+++ commands/avatar.js
@@ -4,9 +4,10 @@ module.exports = {
     aliases: ["av", "ava"],
     description: 'Shows a users avatar',
     execute(message, args) {
-        let men = message.member;
-        if (args[0]) men =  message.mentions.members.first() || message.guild.members.cache.find(m => m.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || 
-        message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(m => m.user.tag.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member;
+        let men;
+        men = message.member
+        if (args[0]) men = message.mentions.members.first() || message.guild.members.cache.find(m => m.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || 
+        message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(m => m.user.tag.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(m => m.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member; 
        
         let embed = new Discord.MessageEmbed()
             .setTitle(`**${men.user.tag}**, ${men.user.id === message.member.user.id ? "Your avatar." : "Their avatar."}`)
