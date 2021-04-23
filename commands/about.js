6cc1efe7d13ea6b64fb975975a701519ade36818
orihuu <81810945+orihuu@users.noreply.github.com>
2021-04-23 11:51:49 +0200

    updated tos and about cmd

--- commands/about.js
+++ commands/about.js
@@ -9,13 +9,13 @@ module.exports = {
         Embed.setTitle(`**ello! I'm Fokushi. Or you can call me Foxu for short.**`)
         Embed.setDescription(`I'm a very **sarcastic** bot, i was originally made by <@814539604879081532> but i was soon to have another developer called <@486396074282450946>. They're both really nice, and great dev's. I was originally a project made to be very big, but turns out both my devs have much to learn about coding. so we'll both have to wait until they're ready to release it for the public. I hope im awesome when they do! you know, i sometimes may to sarcastic or rude when responding to your commands, but I appreciate you using me and liking me. I hope to see you when im public. I love you all! 
         
-My statistics 
+✦ **My statistics**
 
-I'm babysitting ${bot.users.cache.size} users daily.
+I'm babysitting **${bot.users.cache.size}** users daily.
 
-I help out in ${bot.guilds.cache.size} servers everyday.
+I help out in **${bot.guilds.cache.size}** servers everyday.
 
-I can help if you shout one of my ${bot.commands.size} commands.
+I can help if you shout one of my **${bot.commands.size}** commands.
  
 Imagine being developed by <@814539604879081532>, and <@486396074282450946>. couldn't be me, huh?
 
