70e9a834aad94cb821141c2194b31fd00ad6f2dc
orihuu <81810945+orihuu@users.noreply.github.com>
2021-04-25 08:29:57 +0200

    do do do do

---
+++ commands/Developer/status.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'status',
+    aliases: ['s', 'stats'],
+    description: 'My current status.',
+    execute(message, args) {
+        let Embed = new Discor
+if (!config.ids.developerID.includes(message.author.id)) return;d.MessageEmbed()
+        Embed.setDescription(`
+Hi! This is my current status.
+Version: ${config.botver}
+Users: ${bot.users.cache.size}
+
+Everything seems to be fine.`)
+        Embed.setTimestamp()
+        Embed.setColor(`GREY`);
+        message.channel.send(Embed)
+    }
+};
