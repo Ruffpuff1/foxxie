ad671cbb5cfc8ea9f1d85694ac95f21d4940c5ef
orihuu <81810945+orihuu@users.noreply.github.com>
2021-04-25 16:45:23 +0200

    monj

--- commands/Developer/status.js
+++ commands/Developer/status.js
@@ -6,15 +6,40 @@ module.exports = {
     aliases: ['s', 'stats'],
     description: 'My current status.',
     execute(message, args, bot) {
+        
 if (!config.ids.developerID.includes(message.author.id)) return;
+let days = 0
+let week = 0
+let uptime = '';
+let totalSeconds = (bot.uptime / 1000)
+let hours = Math.floor(totalSeconds / 3600)
+totalSeconds %= 3600
+let minutes = Math.floor(totalSeconds / 60)
+let seconds = Math.floor(totalSeconds % 60)
+if (hours > 24) {
+days = days + 1
+hours = 0
+        }
+if (week - 0) {
+uptime += `${week} week, `
+        }
+if (minutes > 60) {
+minutes = 0;
+        }
+uptime += `**${days}** days, **${hours}** hours, **${minutes}** minutes and **${seconds}** seconds`
+
 let Embed = new Discord.MessageEmbed()
 
 Embed.setDescription(`
 Hi! This is my current status.
-Version: ${config.botver}
-Users: ${bot.users.cache.size}
 
-Everything seems to be fine.`)
+Version: **\`${config.botver}\`**
+
+Users: **\`${bot.users.cache.size}\`**
+
+Uptime: **\`${uptime}\`**
+
+\`Everything seems to be fine.\``)
 Embed.setTimestamp()
 Embed.setColor(`GREY`);
 message.channel.send(Embed)
