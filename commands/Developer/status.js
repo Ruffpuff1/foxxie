9c607a1a992cb5a6bb811c1557b6b82d0c11a531
orihuu <81810945+orihuu@users.noreply.github.com>
2021-04-25 08:34:55 +0200

    test

--- commands/Developer/status.js
+++ commands/Developer/status.js
@@ -5,16 +5,16 @@ module.exports = {
     aliases: ['s', 'stats'],
     description: 'My current status.',
     execute(message, args) {
-        let Embed = new Discor
+let Embed = new Discor
 if (!config.ids.developerID.includes(message.author.id)) return;d.MessageEmbed()
-        Embed.setDescription(`
+Embed.setDescription(`
 Hi! This is my current status.
 Version: ${config.botver}
 Users: ${bot.users.cache.size}
 
 Everything seems to be fine.`)
-        Embed.setTimestamp()
-        Embed.setColor(`GREY`);
-        message.channel.send(Embed)
+Embed.setTimestamp()
+Embed.setColor(`GREY`);
+message.channel.send(Embed)
     }
 };
