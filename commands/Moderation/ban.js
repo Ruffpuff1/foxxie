1dd98521e8c76759cc93383d5f3f89b973557553
Ruffpuff1 <73779441+Ruffpuff1@users.noreply.github.com>
2021-04-25 13:53:45 -0700

    Soft ban

--- commands/Moderation/ban.js
+++ commands/Moderation/ban.js
@@ -3,23 +3,26 @@ const Discord = require('discord.js');
 module.exports = {
     name: "ban",
     aliases: ["yeet"],
-    description: "Bans the specified user from the server",
+    description: "Bans the specified user from the server. Add -s or -soft to the ban reason to ban a user and then immediently unban them, clearing all of their messages.",
     permissions: 'BAN_MEMBERS',
 
     async execute(message, args, bot){
         let mentionMember = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
-        mentionMember.ban()
         if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.reply(`Haha, funny. You’re trying to ban with me without perms. Didn't think someone could be that stupid.`);
         if(!mentionMember) return message.reply(` Oh my, we have a real Einstein right here, folks. Mate, listen. You’ve got to specify the member you wish to ban.`);
         let reason = args.slice(1).join(" ");
         if(!reason) reason = "Oops, the moderator that did this ban didnt supply a reason."; 
         if(mentionMember.id === message.author.id) return message.reply(`haha, i see you're trying to be funny, you can't ban yourself with me.`);
 
+        let reg = /\-soft\s*|-s\s*/gi
+        let tru = reg.test(message.content)
+        updatedRes = reason.replace(reg, '')
+
         let Embed = new Discord.MessageEmbed()
         Embed.setTitle(`You have been banned.`)
-        Embed.setDescription(` You have been forever banned from ${message.guild.name}. Sorry, mate. It’s nothing personal, but that’s the way the cookie crumbles.
+        Embed.setDescription(`You have been forever banned from ${message.guild.name}. Sorry, mate. It’s nothing personal, but that’s the way the cookie crumbles.
 
-Reason: ${reason}
+Reason: ${updatedRes}
     `)
         Embed.setTimestamp()
         Embed.setAuthor(mentionMember.user.tag, mentionMember.user.displayAvatarURL())
@@ -27,8 +30,12 @@ Reason: ${reason}
         Embed.setColor("#f59dcc")
 
         if(mentionMember.bannable){
-            mentionMember.send(Embed).catch(error => message.channel.send(`oh, i think the member you banned had their DMs off, because i couldnt DM them.`))
-            .then(m => mentionMember.ban({reason}));
+
+            mentionMember.send(Embed).catch(err => message.channel.send(`oh, i think the member you banned had their DMs off, because i couldnt DM them.`))
+
+            if (tru) mentionMember.ban({reason:updatedRes, days: 1}).then(message.guild.members.unban(mentionMember)).catch(console.error)
+
+            if (!tru) mentionMember.ban({updatedRes})
 
             message.channel.send(`:white_check_mark: ${message.author.username} has banished ${mentionMember.user.username} from the server. They will never be able to return. Adios!`)
             let logChannel = message.guild.channels.cache.get("822454708894695444")
@@ -37,7 +44,7 @@ Reason: ${reason}
 	embed.setTitle(`Member banned.`)
 	embed.setDescription(`**${mentionMember.user}** was banned from the server.`)
 	embed.addFields(
-        { name: 'Reason', value: `${reason}` },  { name: 'Moderator', value: `${message.author} (ID: ${message.author.id})` },
+        { name: 'Reason', value: `${updatedRes}` },  { name: 'Moderator', value: `${message.author} (ID: ${message.author.id})` },
 
         { name: 'Location', value: `${message.channel}` }, 
 
@@ -46,7 +53,7 @@ Reason: ${reason}
 	
 	embed.setTimestamp()
 	embed.setFooter('Why are you reading this?', message.author.displayAvatarURL({ format: "png", dynamic: true, size: 4096}));
-logChannel.send(embed)
+    if (logChannel) logChannel.send(embed)
     
     
 } else {
