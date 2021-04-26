98fd1e0de01ecd9e3bd670c66b4a5af8f1fd9950
orihuu <81810945+orihuu@users.noreply.github.com>
2021-04-26 11:05:13 +0200

    boooooooooooooooolts

--- commands/Fun/pikachu.js
+++ commands/Fun/pikachu.js
@@ -4,7 +4,7 @@ module.exports = {
     name: 'pikachu',
     aliases: ['pika', 'pikapika'],
     description: 'pika pika.',
-    execute(message, args) {
+    execute(message, args, bot) {
         let Embed = new Discord.MessageEmbed()
         Embed.setImage(`https://images-ext-1.discordapp.net/external/ssgTbUpw1ngMy0j24_jWB1ZYZLAnsUmyyWd99AA7kbg/https/media.discordapp.net/attachments/713900746201890896/830949617607770132/image0.gif`)
         Embed.setColor(`YELLOW`);
