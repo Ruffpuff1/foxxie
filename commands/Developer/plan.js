7fbdcb4b34fef478147f49c6a6caee25891a42d7
orihuu <81810945+orihuu@users.noreply.github.com>
2021-04-26 10:26:12 +0200

    plan.js

--- commands/Developer/plan.js
+++ commands/Developer/plan.js
@@ -50,54 +50,47 @@ out of server bans
 out of server avs
 specific user purges
 
-April:
+`)
+Embed.addField(`April:
 Softbans
-Autoroles
-
-May:
+Autoroles`)
+Embed.addField(`May:
 more rp commands
 out of server bans
 poll
 prefix set command
-unban
-
-June:
+unban`)
+Embed.addField(`June:
 out of server user showing
 out of server avs
 specific user purges
 lock and unlock
-role info
-
-July:
+role info`)
+Embed.addField(`July:
 antiinvite
 urban
 reminders
 rr
-music
-
-August:
+music`)
+Embed.addField(`August:
 warnings
 afk
 welcome channel and goodbye channel
 welcome message and goodbye messsage command
 disboard channel command
 disboard message cmd
-Making a lot of non multiple guild commands multiple guilds
-
-September:
+Making a lot of non multiple guild commands multiple guilds`)
+Embed.addField(`September:
 log channel command
 bugreport
 ticket system
-rest of the remaining commands to make multiple guild and also the remaining commands to add
-
+rest of the remaining commands to make multiple guild and also the remaining commands to add`)
+Embed.addField(`
 October:
 fix any bugs, update any commands before 15th october
 Apply for top.gg before 15th
 Hopefully get 100 or 50 servers
-if 100 then apply for verification
-
-
-`)
+if 100 then apply for verification`)
 Embed.setTimestamp()
 Embed.setColor(`GREY`);
 message.channel.send(Embed)
