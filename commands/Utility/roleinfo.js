0f254efce2a867e16a6f8cdf25478dc465c9bc22
orihuu <81810945+orihuu@users.noreply.github.com>
2021-04-28 11:40:42 +0200

    roleinfo testing

--- commands/Utility/roleinfo.js
+++ commands/Utility/roleinfo.js
@@ -1,15 +1,17 @@
-const Discord = require('discord.js');
-
-module.exports = {
-    name: 'roleinfo',
-    aliases: ['rolei', 'role'],
-    description: 'Shows information about that specific role.',
-    execute(message, args) {
-        let Embed = new Discord.MessageEmbed()
-        Embed.setDescription(`
-work in progress`)
-        Embed.setTimestamp()
-        Embed.setColor(`#2f008c`);
-        message.channel.send(Embed)
-    }
-};
+const { MessageEmbed } = require('discord.js')
+module.exports.aliases = ['roleinfo', 'ri']
+module.exports.run = (bot, msg) => {
+  const args = msg.content.split(' ')
+  let role = msg.mentions.roles.first() || msg.guild.roles.cache.get(args[0]) || msg.guild.roles.cache.find(role => role.name === args[0])
+  if (!role) role = msg.member.roles.highest
+  const embed = new MessageEmbed()
+    .setColor(role.hexColor)
+    .setTitle(`Information about ${role.name}`)
+    .addField('Members', role.members.size)
+    .addField('Color', role.hexColor)
+    .addField('Creation Date', role.createdAt.toDateString())
+    .addField('Modifiablity', role.editable.toString())
+    .addField('Manage Access', role.managed.toString())
+    .addField('ID', role.id)
+  msg.channel.send(embed)
+}
