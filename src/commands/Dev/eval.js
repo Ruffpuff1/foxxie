b325e2fdfc648fa37a8daee08cd369e70742b100
Ruffpuff1 <73779441+Ruffpuff1@users.noreply.github.com>
2021-04-27 20:39:20 -0700

    e

--- src/commands/Dev/eval.js
+++ src/commands/Dev/eval.js
@@ -1,5 +1,6 @@
 const config = require('../../../lib/config')
 const moment = require('moment')
+const { flags: { async, depth, silent } } = require('../../../lib/util/constants')
 const constant = require('../../../lib/util/constants')
 const { roleplayCommand } = require('../../../lib/structures/roleplayCommands')
 const roleplay = require('../../../lib/structures/roleplayCommand')
@@ -9,12 +10,18 @@ const Discord = require('discord.js')
 module.exports = {
     name: 'eval',
     aliases: ['ev'],
-    usage: 'fox eval [code]',
+    usage: 'fox eval [code] (-async|-a) (-silent|-s)',
     category: 'developer',
-    execute: async (lang, message, args, client) => {
+    execute: async (lang, message, args) => {
+
+        let tru = silent.test(message.content)
+        let asyn = async.test(message.content)
+        let codein = args.slice(0).join(" ").replace(silent, '')
+        codein = codein.replace(async, '')
+        asyn ? codein = `(async () => {\n${codein}\n})();` : ''
+
 
         if (config.devs.includes(message.author.id)) {
-            let codein = args.slice(0).join(" ")
             if (!codein.toLowerCase().includes("token")) {
                 try {
                     let code = eval(codein)
@@ -22,9 +29,9 @@ module.exports = {
                     if (codein.legth < 1 && !codein)
                         return message.channel.send(`${lang.COMMAND_EVAL_OUTPUT}\n\`\`\`javascript\n${COMMAND_EVAL_UNDEFINED}\n\`\`\``)
                     if (typeof code !== "string")
-                        code = require("util").inspect(code, { depth : 2 } )
+                        code = require("util").inspect(code, { depth : 0 } )
                     
-                    message.channel.send(`\n${lang.COMMAND_EVAL_OUTPUT}\n\`\`\`javascript\n${code.length > 1024 ? `${lang.COMMAND_EVAL_OVER}` : code}\n\`\`\`\n${lang.COMMAND_EVAL_TYPE}\n\`\`\`javascript\n${type}\n\`\`\``)
+                    if (!tru) message.channel.send(`\n${lang.COMMAND_EVAL_OUTPUT}\n\`\`\`javascript\n${code.length > 1024 ? `${lang.COMMAND_EVAL_OVER}` : code}\n\`\`\`\n${lang.COMMAND_EVAL_TYPE}\n\`\`\`javascript\n${type}\n\`\`\``)
                         .then(resultMessage => {
                             const time = resultMessage.createdTimestamp - message.createdTimestamp
                             const timeMs = resultMessage.createdTimestamp*1000 - message.createdTimestamp*1000
@@ -33,7 +40,7 @@ module.exports = {
                     )
                 } catch(e) {
                     let eType = typeof e
-                    message.channel.send(`\n${lang.COMMAND_EVAL_OUTPUT}\n\`\`\`javascript\n${e.length > 1024 ? `${lang.COMMAND_EVAL_OVER}` : e}\n\`\`\`\n${lang.COMMAND_EVAL_TYPE}\n\`\`\`javascript\n${eType}\n\`\`\``)
+                    if (!tru) message.channel.send(`\n${lang.COMMAND_EVAL_OUTPUT}\n\`\`\`javascript\n${e.length > 1024 ? `${lang.COMMAND_EVAL_OVER}` : e}\n\`\`\`\n${lang.COMMAND_EVAL_TYPE}\n\`\`\`javascript\n${eType}\n\`\`\``)
                         .then(resultMessage => {
                             const time = resultMessage.createdTimestamp - message.createdTimestamp
                             const timeMs = resultMessage.createdTimestamp*1000 - message.createdTimestamp*1000
--- src/monitors/rero.js
+++ src/monitors/rero.js
@@ -1,17 +1,22 @@
+const { serverSettings } = require('../../lib/settings')
 module.exports = {
     name: 'rero',
     execute: async (reaction, user, act) => {
 
-        let reros = [ { emoji: '824751934539825232', message: '836790354858606644', role: '825929686634856459' } ]
+        console.log('got reaction')
+
+        let server = await serverSettings(reaction.message)
+        if (server == null) return;
+        if (server.reros == null) return;
+
         const member = reaction.message.guild ? await reaction.message.guild.members.fetch(user).catch(() => null) : null;
         if (!member) return;
 
-        for (let rero of reros){
-            console.log('emoji:', rero.emoji)
-            console.log('message:', rero.message)
-            if (reaction.emoji.id === rero.emoji && reaction.message.id === rero.message) {
-                console.log('role:', rero.role)
-                let role = reaction.message.guild.roles.cache.find(r => r.id === rero.role)
+        for (let rero of server.reros){
+
+            if (reaction.emoji.id === rero[0].emoji || reaction.emoji.name === rero[0].emoji && reaction.message.id === rero[0].message) {
+
+                let role = reaction.message.guild.roles.cache.find(r => r.id === rero[0].role)
 
                 if (act === 'add') member.roles.add(role)
                 if (act === 'remove') member.roles.remove(role)
