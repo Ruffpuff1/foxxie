9db9051c5b6dd9396b1829ef23b64ce27f1ad3dd
Ruffpuff1 <73779441+Ruffpuff1@users.noreply.github.com>
2021-05-03 16:03:51 -0700

    Update status.js

--- events/status.js
+++ events/status.js
@@ -16,12 +16,13 @@ module.exports.status = async (bot) => {
     if (!message) return;
 
     const embed = new MessageEmbed()
+        .setTitle(`${bot.user.username} v${botver}`)
         .setColor(guild.me.displayColor)
         .setTimestamp()
-        .addField('Version:', `\`v${botver}\``, false)
-        .addField('Users:', `\`${bot.users.cache.size}\``, false)
-        .addField('Guilds:', `\`${bot.guilds.cache.size}\``, false)
-        .addField('Status:', `\`Everything seems to be fine.\``, false)
+        .addField('**Stats**:', `**${bot.users.cache.size.toLocaleString()}** users and **${bot.guilds.cache.size.toLocaleString()}** guilds.`, false)
+        .addField('**Memory**:', `**${Math.floor(process.memoryUsage().heapUsed / 1024 / 1024).toLocaleString()}** megabytes.`)
+        .addField('**Uptime**:', `**${Math.floor(bot.uptime / 1000).toLocaleString()}** seconds.`, false)
+        .addField('**Message**:', `\`Everything seems to be fine.\``, false)
 
 
     message.edit(`Hi this is my current status.`, {embed:embed})
