86237f5706618172e3226e2e83d1f0e52769aa4d
Ruffpuff1 <73779441+Ruffpuff1@users.noreply.github.com>
2022-03-03 16:28:29 -0800

    feat(apps): add kettu

---
+++ apps/kettu/src/lib/i18n/Keys/Commands/General.ts
@@ -0,0 +1,4 @@
+import { T } from '#types/Utils';
+
+export const DonateDescription = T('commands/general:donateDescription');
+export const DonateHeader = T('commands/general:donateHeader');
---
+++ apps/kettu/src/lib/i18n/Keys/index.ts
@@ -0,0 +1,3 @@
+export * as Commands from './Commands';
+export * as Globals from './Globals';
+export * as System from './System';
