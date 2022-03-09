2a0d8e7cee115c198fea4d6eee560e631b120f00
Ruffpuff1 <73779441+Ruffpuff1@users.noreply.github.com>
2022-03-09 13:49:42 -0800

    feat(kettu): add stats command

--- apps/kettu/src/lib/i18n/Keys/Commands/General.ts
+++ apps/kettu/src/lib/i18n/Keys/Commands/General.ts
@@ -1,4 +1,13 @@
-import { T } from '#types/Utils';
+import { FT, T } from '#types/Utils';
 
 export const DonateDescription = T('commands/general:donateDescription');
 export const DonateHeader = T('commands/general:donateHeader');
+export const StatsDescription = T('commands/general:statsDescription');
+export const StatsGeneral = FT<{ channels: number; guilds: number; nodeJs: string; users: number; version: string }>('commands/general:statsGeneral');
+export const StatsTitles = T<{
+    stats: string;
+    uptime: string;
+    usage: string;
+}>('commands/general:statsTitles');
+export const StatsUptime = FT<{ client: string; host: string; process: string }>('commands/general:statsUptime');
+export const StatsUsage = FT<{ cpuLoad: string; ramUsed: number; ramTotal: number }>('commands/general:statsUsage');
