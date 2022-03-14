c9386d1aec44b78c8378f2e90f0e416b9cca50e9
Ruff <73779441+Ruffpuff1@users.noreply.github.com>
2022-03-14 07:23:19 -0700

    chore: remove pride commands in favor of kettu (#147)

--- apps/foxxie/src/lib/i18n/LanguageKeys/Commands/index.ts
+++ apps/foxxie/src/lib/i18n/LanguageKeys/Commands/index.ts
@@ -4,6 +4,5 @@ export * as General from './General';
 export * as Info from './Info';
 export * as Misc from './Misc';
 export * as Moderation from './Moderation';
-export * as Pride from './Pride';
 export * as Social from './Social';
 export * as Websearch from './Websearch';
