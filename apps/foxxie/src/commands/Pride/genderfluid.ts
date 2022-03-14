d7d191c3559c0caa0094c849a73b57d1f67ffe45
Ruffpuff1 <73779441+Ruffpuff1@users.noreply.github.com>
2022-03-14 07:22:25 -0700

    chore: remove pride commands in favor of kettu

--- apps/foxxie/src/commands/Pride/genderfluid.ts
+++
@@ -1,9 +0,0 @@
-import { LGBTCommand } from '#lib/structures';
-import { ApplyOptions } from '@sapphire/decorators';
-import { LanguageKeys } from '#lib/i18n';
-
-@ApplyOptions<LGBTCommand.Options>({
-    description: LanguageKeys.Commands.Pride.GenderfluidDescription,
-    detailedDescription: LanguageKeys.Commands.Pride.GenderfluidDetailedDescription
-})
-export class UserCommand extends LGBTCommand {}
