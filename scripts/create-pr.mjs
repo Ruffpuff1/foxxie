a08c535045216c6eee5d2fda305980d09d5d4be9
Ruffpuff1 <73779441+Ruffpuff1@users.noreply.github.com>
2022-03-15 17:00:52 -0700

    chore: try out publish workflow

---
+++ scripts/create-pr.mjs
@@ -0,0 +1,48 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/restrict-template-expressions */
+import { Octokit } from '@octokit/action';
+import { readFile } from 'node:fs/promises';
+
+const octokit = new Octokit();
+const [OWNER, REPOSITORY] = process.env.GITHUB_REPOSITORY.split('/');
+
+const arg = process.argv[2];
+if (!arg) throw new Error('Argument must be provided');
+
+const [, packageName] = arg.split('=');
+if (!packageName) throw new Error('Couldnt read packageName');
+
+const packageJson = JSON.parse(await readFile(new URL(`../../packages/${packageName}package.json`, import.meta.url), { encoding: 'utf8' }));
+
+console.log('👀 Getting the previous release version');
+const previousReleases = await octokit.repos.listReleases({
+    owner: OWNER,
+    repo: REPOSITORY
+});
+
+// Releases are sorted from newest to oldest, this should work™️
+const previousRelease = previousReleases.data.find(release => !release.draft);
+console.log('👀 Previous release version:', previousRelease?.tag_name);
+
+const pullRequestBody = [
+    '**Please describe the changes this PR makes and why it should be merged:**',
+    '',
+    `This pull request bumps ${packageName} from **${previousRelease?.tag_name ?? 'unknown'}** to **${packageJson.version}**.`,
+    '',
+    '⚠️ **Do not change the commit message when merging. It must stay in the format `chore(release): ...`!**',
+    '⚠️ Maintainers, make sure everything is alright in this PR before merging it. This is still a beta test, so things may break.'
+];
+
+console.log(`🎉 Creating pull request for ${packageName} ${packageJson.version}`);
+
+const pullRequest = await octokit.pulls.create({
+    base: 'main',
+    // The format must stay in sync with the one in create-pr-for-release-and-publish.yml
+    head: `chore/release/${packageJson.version}`,
+    owner: OWNER,
+    repo: REPOSITORY,
+    maintainer_can_modify: true,
+    title: `chore(release): ${packageJson.version} 🎉`,
+    body: pullRequestBody.join('\n')
+});
+
+console.log(`✅ Done! Created pull request ${pullRequest.data.html_url}`);
