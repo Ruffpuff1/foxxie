8b96a02e08618575e249a456ee10ad2125c3bcb3
Ruffpuff1 <73779441+Ruffpuff1@users.noreply.github.com>
2022-03-27 16:25:01 -0700

    feat(env): turn fields of the class to static properties

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -1,14 +1,14 @@
 import type { Env, BooleanString, IntegerString } from './types';
 
 class EnvParse {
-    public string<K extends keyof Env>(key: K): string {
+    public static string<K extends keyof Env>(key: K): string {
         const value = process.env[key];
         if (value) return value;
 
         throw new Error(`Could not resolve "${key}" to string value.`);
     }
 
-    public int<K extends keyof Env>(key: K): number {
+    public static int<K extends keyof Env>(key: K): number {
         const value = process.env[key];
         if (!value) throw new Error(`Could not resolve "${key}" to integer value.`);
 
@@ -17,24 +17,24 @@ class EnvParse {
         return parsed;
     }
 
-    public boolean<K extends keyof Env>(key: K): boolean {
+    public static boolean<K extends keyof Env>(key: K): boolean {
         const value = process.env[key];
         if (value === 'true') return true;
         if (value === 'false') return false;
         throw new Error(`Could not resolve "${key}" to boolean value.`);
     }
 
-    public array<K extends keyof Env>(key: K, split = ' '): string[] {
+    public static array<K extends keyof Env>(key: K, split = ' '): string[] {
         const value = process.env[key];
         if (!value) throw new Error(`Could not resolve "${key}" to array value.`);
         return value.split(split);
     }
 
-    public exists<K extends keyof Env>(...keys: K[]): boolean {
+    public static exists<K extends keyof Env>(...keys: K[]): boolean {
         return keys.every(k => {
             const value = process.env[k];
             return value !== undefined && value.length !== 0;
-        })
+        });
     }
 }
 
