f9e9a598fdc8ed3256cb1a1a7a1d21a7cfc24736
Ruff <73779441+Ruffpuff1@users.noreply.github.com>
2022-03-27 20:11:01 -0700

    feat: replace turborepo with nx (#283)

--- packages/fetch/src/index.ts
+++ packages/fetch/src/index.ts
@@ -1,4 +1,5 @@
-import { HttpMethod, Request } from './Request';
+import { Request } from './Request';
+import { HttpMethodEnum } from './types';
 
 /**
  * Fetch a link using undici
@@ -6,5 +7,5 @@ import { HttpMethod, Request } from './Request';
  * @param method The method to fetch with, defaults to GET.
  * @returns Request The {@link Request} class.
  */
-export const fetch = (url: string | URL, method?: HttpMethod) => new Request(url, method);
-export { HttpMethodEnum, HttpMethod } from './Request';
+export const fetch = (url: string | URL, method?: `${HttpMethodEnum}`) => new Request(url, method);
+export { HttpMethodEnum };
