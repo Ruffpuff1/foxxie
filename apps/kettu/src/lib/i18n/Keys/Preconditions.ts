62bfd457e7b081fa90fba9482b0655ba7791acc3
Ruff <reeseharlak@icloud.com>
2022-04-01 10:30:23 -0700

    feat(kettu): add enabled precondition (#353)

--- apps/kettu/src/lib/i18n/Keys/Preconditions.ts
+++ apps/kettu/src/lib/i18n/Keys/Preconditions.ts
@@ -1,3 +1,4 @@
-import { FT } from "#types/utils";
+import { FT } from '#types/utils';
 
 export const ClientPermissions = FT<{ missing: string[] }>('preconditions:clientPermissions');
+export const Enabled = FT<{ name: string }>('preconditions:enabled');
