5dddb1e99744678e25ad91deeed1fbfa245dc688
Ruffpuff1 <73779441+Ruffpuff1@users.noreply.github.com>
2022-04-01 20:14:32 -0700

    feat(website): move vote page

--- website/foxxie.xyz/src/pages/kettu/vote/topgg.tsx
+++ website/foxxie.xyz/src/pages/kettu/vote/topgg.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next';
-import { TopggVotepage } from '../../components/pages/TopggVotepage';
-import { KETTU_ID } from '../../utils/constants';
+import { TopggVotepage } from '../../../components/pages/TopggVotepage';
+import { KETTU_ID } from '../../../utils/constants';
 
 const Vote: NextPage = () => {
     return (
