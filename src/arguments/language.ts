ab7f36705b25bf0d46b2aaf94857c81dbce1c641
Reese <reeseharlak@icloud.com>
2022-06-20 16:25:13 -0700

    fix: update language detection (#570)

--- src/arguments/language.ts
+++ src/arguments/language.ts
@@ -6,13 +6,13 @@ import i18next from 'i18next';
 
 export class UserArgument extends Argument<LanguageString> {
     public async run(parameter: string, context: ArgumentContext): Promise<Result<LanguageString, UserError>> {
-        const { languages } = i18next;
-        if (languages.includes(parameter)) return this.ok(parameter as LanguageString);
+        const { preload } = i18next.options;
+        if ((preload as string[]).includes(parameter)) return this.ok(parameter as LanguageString);
 
         return this.error({
             parameter,
             identifier: LanguageKeys.Arguments.Language,
-            context: { ...context, list: languages }
+            context: { ...context, list: preload }
         });
     }
 }
